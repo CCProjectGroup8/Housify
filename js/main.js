32387cca07675ccdcbec2636359aefb1cf905f86
longlongCU <long.long@columbia.edu>
2017-05-06 23:05:46 -0400

    style almost, need to fix lambda at first

--- js/main.js
+++ js/main.js
@@ -600,13 +600,13 @@ function houseDetail() {
 function initMap(){
 
     init_position = [];
-    init_position['lat'] = -33.865427;
-    init_position['lng'] = 151.196123;
+    init_position['lat'] = 40.8089675;
+    init_position['lng'] = -73.9599175;
 
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 1,
+        zoom: 10,
         center: {lat: init_position['lat'], lng: init_position['lng']},
-        mapTypeId: 'terrain'
+        styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
     });
     // console.log(coords);
     // console.log(coords.length);
@@ -618,7 +618,7 @@ function initMap(){
                 init_position['lat'] = position.coords.latitude;
                 init_position['lng'] = position.coords.longitude;
                 console.log(init_position);
-                map.setZoom(8);
+                map.setZoom(11);
                 map.setCenter({lat: init_position['lat'], lng: init_position['lng']});
             }
         )
