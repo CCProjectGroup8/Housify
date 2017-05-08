b72908638fc9660c0588106dafc80d6af9811d2e
Ubuntu <ubuntu@ip-172-31-30-242.ec2.internal>
2017-05-08 01:32:17 +0000

    ML of hengshen

--- ML/postWork.py
+++ ML/postWork.py
@@ -6,11 +6,14 @@ from pyspark import SparkConf, SparkContext
 from pyspark.mllib.recommendation import ALS
 import math
 import boto3
+import getRes
+
+result = getRes.getRes()
 
 # get table heree
 dynamodb = boto3.resource('dynamodb',
-                          aws_access_key_id = 'AKIAJXGZJRT7NCEH4PTQ',
-                          aws_secret_access_key = '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',
+                          aws_access_key_id = '...',
+                          aws_secret_access_key = '...',
                           region_name = 'us-east-1'
                           )
 recommendtable= dynamodb.Table('recommendresult')
@@ -65,50 +68,67 @@ def get_counts_and_averages(ID_and_ratings_tuple):
 movie_ID_with_ratings_RDD = (complete_ratings_data.map(lambda x: (x[1], x[2])).groupByKey())
 movie_ID_with_avg_ratings_RDD = movie_ID_with_ratings_RDD.map(get_counts_and_averages)
 movie_rating_counts_RDD = movie_ID_with_avg_ratings_RDD.map(lambda x: (x[0], x[1][0]))
-
-new_user_ID = 111111
-
-new_user_ratings = [
-   (111111, 1089542, 0.9),
-   (111111, 16613394, 0.5),
-   (111111, 16649118, 0.8)]
-
-new_user_ratings_RDD = sc.parallelize(new_user_ratings)
-print 'New user ratings: %s' % new_user_ratings_RDD.take(10)
-
-complete_data_with_new_ratings_RDD = complete_ratings_data.union(new_user_ratings_RDD)
-
-new_ratings_model = ALS.train(complete_data_with_new_ratings_RDD, best_rank, seed=seed, 
-                              iterations=iterations, lambda_=regularization_parameter)
-
-new_user_ratings_ids = map(lambda x: x[1], new_user_ratings) # get just movie IDs
-# keep just those not on the ID list (thanks Lei Li for spotting the error!)
-new_user_unrated_movies_RDD = (complete_movies_data.filter(lambda x: x[0] not in new_user_ratings_ids).map(lambda x: (new_user_ID, x[0])))
-
-# Use the input RDD, new_user_unrated_movies_RDD, with new_ratings_model.predictAll() to predict new ratings for the movies
-new_user_recommendations_RDD = new_ratings_model.predictAll(new_user_unrated_movies_RDD)
-
-# Transform new_user_recommendations_RDD into pairs of the form (Movie ID, Predicted Rating)
-new_user_recommendations_rating_RDD = new_user_recommendations_RDD.map(lambda x: (x.product, x.rating))
-print new_user_recommendations_rating_RDD.take(5)
-new_user_recommendations_rating_title_and_count_RDD = \
-    new_user_recommendations_rating_RDD.join(complete_movies_titles).join(movie_rating_counts_RDD)
-new_user_recommendations_rating_title_and_count_RDD.take(3)
-
-new_user_recommendations_rating_title_and_count_RDD = \
-    new_user_recommendations_rating_title_and_count_RDD.map(lambda r: (r[1][0][1], r[1][0][0], r[1][1]))
-    
-#top_movies = new_user_recommendations_rating_title_and_count_RDD.filter(lambda r: r[2]>=25).takeOrdered(25, key=lambda x: -x[1])
-top_movies = new_user_recommendations_rating_RDD.takeOrdered(25, key=lambda x: -x[1])
-
-
-print map(str, top_movies)
-
-for i in xrange(len(top_movies)):
-	pass
-#	print top_movies[i][1]	
 	
 
-#top_movies = new_user_recommendations_rating_RDD.takeOrdered(25, key=lambda x: -x[1])
-#
-#print top_movies
+for key in result:		
+	new_user_ID = key
+	
+	new_user_ratings = result[key]
+	
+	new_user_ratings_RDD = sc.parallelize(new_user_ratings)
+	print 'New user ratings: %s' % new_user_ratings_RDD.take(10)
+	
+	complete_data_with_new_ratings_RDD = complete_ratings_data.union(new_user_ratings_RDD)
+	
+	new_ratings_model = ALS.train(complete_data_with_new_ratings_RDD, best_rank, seed=seed, 
+	                              iterations=iterations, lambda_=regularization_parameter)
+	
+	new_user_ratings_ids = map(lambda x: x[1], new_user_ratings) # get just movie IDs
+	# keep just those not on the ID list (thanks Lei Li for spotting the error!)
+	new_user_unrated_movies_RDD = (complete_movies_data.filter(lambda x: x[0] not in new_user_ratings_ids).map(lambda x: (new_user_ID, x[0])))
+	
+	# Use the input RDD, new_user_unrated_movies_RDD, with new_ratings_model.predictAll() to predict new ratings for the movies
+	new_user_recommendations_RDD = new_ratings_model.predictAll(new_user_unrated_movies_RDD)
+	
+	# Transform new_user_recommendations_RDD into pairs of the form (Movie ID, Predicted Rating)
+	new_user_recommendations_rating_RDD = new_user_recommendations_RDD.map(lambda x: (x.product, x.rating))
+	print new_user_recommendations_rating_RDD.take(5)
+	new_user_recommendations_rating_title_and_count_RDD = \
+	    new_user_recommendations_rating_RDD.join(complete_movies_titles).join(movie_rating_counts_RDD)
+	new_user_recommendations_rating_title_and_count_RDD.take(3)
+	
+	new_user_recommendations_rating_title_and_count_RDD = \
+	    new_user_recommendations_rating_title_and_count_RDD.map(lambda r: (r[1][0][1], r[1][0][0], r[1][1]))
+	    
+	#top_movies = new_user_recommendations_rating_title_and_count_RDD.filter(lambda r: r[2]>=25).takeOrdered(25, key=lambda x: -x[1])
+	top_movies = new_user_recommendations_rating_RDD.takeOrdered(25, key=lambda x: -x[1])
+	
+	
+	print map(str, top_movies)
+	
+	for i in xrange(len(top_movies)):
+		pass
+	#	print top_movies[i][1]	
+		
+	
+	#top_movies = new_user_recommendations_rating_RDD.takeOrdered(25, key=lambda x: -x[1])
+	#
+	#print top_movies
+	
+	recresult = {
+		'username' : key
+	}
+	recresult['recommendation'] = {}
+	
+	recresult['recommendation']['houseId'] = []
+	recresult['recommendation']['score'] = []
+
+	for item in result[key]:
+		recresult['recommendation']['houseId'].append(item[1])
+		recresult['recommendation']['score'].append(item[2])
+	print "putrecommend for"
+	print key
+	response = recommendtable.put_item(
+		Item = recresult
+	)		
+
